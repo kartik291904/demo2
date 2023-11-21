bc775163003cfa6238b4f55cd323fa37012dce2b
kartik290419 <kartik290419@gmail.com>
2023-11-21 17:11:17 +0530

    commit

--- Basefiles/__init__.py
+++ Basefiles/__init__.py
@@ -1,2 +1,11 @@
 from . import InitiateDriver
 from . import ConfigReader
+# def prime(num):
+#     for i in range(2,num+1):
+#         for j in range(2,i):
+#             if (i%j)==0:
+#                 break
+#             print(i)
+#         else:
+#             print(i)
+# prime(20)
